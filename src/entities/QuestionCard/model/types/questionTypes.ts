7917be4b2de861c5d70de746f6230b9e6d060caf
David_Gasparyan <david.gasparyan@techwings.com>
2023-08-24 10:01:12 +0400

    changing apple bundle id, new GoogleService-Info.plist file

--- src/entities/QuestionCard/model/types/questionTypes.ts
+++ src/entities/QuestionCard/model/types/questionTypes.ts
@@ -1,14 +1,17 @@
-import {DisplayText} from '@src/shared/types/types';
-
 export type QuestionCardTypes = 'WILD_CARD' | 'Action_CARD' | 'ORDINARY';
+
+export interface Session {
+  challenge: string;
+  question: string[];
+}
 
 export interface QuestionType {
   categoryId: string;
-  question: DisplayText;
   rubricId: string;
   type: QuestionCardTypes;
   id: string;
   createdDate: string;
+  sessions: Session[];
 }
 
 export interface QuestionPreviewType {
