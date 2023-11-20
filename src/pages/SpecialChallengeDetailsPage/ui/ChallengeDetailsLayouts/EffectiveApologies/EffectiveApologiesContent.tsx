512b30daa3e82e6972d48f210434a4e1ee246528
Rustem Baltiyev <r.180200@gmail.com>
2023-11-20 14:38:18 +0300

    added special challenge "Effective Apologies"

--- src/pages/SpecialChallengeDetailsPage/ui/ChallengeDetailsLayouts/EffectiveApologies/EffectiveApologiesContent.tsx
+++ src/pages/SpecialChallengeDetailsPage/ui/ChallengeDetailsLayouts/EffectiveApologies/EffectiveApologiesContent.tsx
@@ -1,7 +1,13 @@
 import React, {memo} from 'react';
 import {View} from 'react-native';
 
-import {ChallengeIntroCard} from '@src/entities/Challenge';
+import {
+  ChallengeCard,
+  ChallengeDescription,
+  ChallengeIntroCard,
+} from '@src/entities/Challenge';
+import {LanguageValueType} from '@src/widgets/LanguageSwitcher';
+import {useTranslation} from 'react-i18next';
 
 interface EffectiveApologiesContentProps {
   data: any;
@@ -12,6 +18,9 @@ export const EffectiveApologiesContent = (
 ) => {
   const {data} = props;
 
+  const {i18n} = useTranslation();
+  const language = i18n.language as LanguageValueType;
+
   let content;
 
   switch (data.type) {
@@ -20,6 +29,91 @@ export const EffectiveApologiesContent = (
         <ChallengeIntroCard title={data.title} description={data.description} />
       );
       break;
+
+    case 'data_1':
+      content = (
+        <ChallengeCard title={data.title[language]}>
+          <ChallengeDescription
+            gradientWordscount={1}
+            description={data.description1[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription
+            gradientWordscount={1}
+            description={data.description2[language]}
+          />
+        </ChallengeCard>
+      );
+      break;
+
+    case 'data_2':
+      content = (
+        <ChallengeCard title={data.title[language]}>
+          <ChallengeDescription
+            description={data.description1[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription
+            description={data.description2[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription
+            description={data.description3[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription
+            description={data.description4[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription
+            description={data.description5[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription
+            description={data.description6[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription
+            description={data.description7[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription description={data.description8[language]} />
+        </ChallengeCard>
+      );
+      break;
+
+    case 'data_3':
+      content = (
+        <ChallengeCard title={data.title[language]}>
+          <ChallengeDescription
+            description={data.description1[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription
+            description={data.description2[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription description={data.description3[language]} />
+        </ChallengeCard>
+      );
+      break;
+
+    case 'data_4':
+      content = (
+        <ChallengeCard title={data.title[language]}>
+          <ChallengeDescription
+            description={data.description1[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription
+            description={data.description2[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription description={data.description3[language]} />
+        </ChallengeCard>
+      );
+      break;
+
     default:
       content = <View />;
   }
