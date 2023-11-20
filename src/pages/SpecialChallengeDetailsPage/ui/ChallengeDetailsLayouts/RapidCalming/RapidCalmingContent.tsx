053084dc4459a08f2aaa59f5a500dcf86d7fc085
Rustem Baltiyev <r.180200@gmail.com>
2023-11-20 17:01:10 +0300

    added special challenge "Rapid Calming"

--- src/pages/SpecialChallengeDetailsPage/ui/ChallengeDetailsLayouts/RapidCalming/RapidCalmingContent.tsx
+++ src/pages/SpecialChallengeDetailsPage/ui/ChallengeDetailsLayouts/RapidCalming/RapidCalmingContent.tsx
@@ -1,7 +1,13 @@
 import React, {memo} from 'react';
 import {View} from 'react-native';
 
-import {ChallengeIntroCard} from '@src/entities/Challenge';
+import {
+  ChallengeCard,
+  ChallengeDescription,
+  ChallengeIntroCard,
+} from '@src/entities/Challenge';
+import {LanguageValueType} from '@src/widgets/LanguageSwitcher';
+import {useTranslation} from 'react-i18next';
 
 interface RapidCalmingContentProps {
   data: any;
@@ -12,12 +18,71 @@ export const RapidCalmingContent = (props: RapidCalmingContentProps) => {
 
   let content;
 
+  const {i18n} = useTranslation();
+  const language = i18n.language as LanguageValueType;
+
   switch (data.type) {
     case 'intro':
       content = (
         <ChallengeIntroCard title={data.title} description={data.description} />
       );
       break;
+
+    case 'data_1':
+      content = (
+        <ChallengeCard title={data.title[language]}>
+          <ChallengeDescription
+            description={data.description1[language]}
+            gradientWordscount={2}
+            isMarginBottom
+          />
+          <ChallengeDescription
+            description={data.description2[language]}
+            gradientWordscount={3}
+            isMarginBottom
+          />
+          <ChallengeDescription
+            description={data.description3[language]}
+            gradientWordscount={1}
+            isMarginBottom
+          />
+          <ChallengeDescription
+            description={data.description4[language]}
+            gradientWordscount={1}
+            isMarginBottom
+          />
+          <ChallengeDescription
+            description={data.description5[language]}
+            gradientWordscount={1}
+          />
+        </ChallengeCard>
+      );
+      break;
+
+    case 'data_2':
+      content = (
+        <ChallengeCard title={data.title[language]}>
+          <ChallengeDescription
+            description={data.description1[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription
+            description={data.description2[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription
+            description={data.description3[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription
+            description={data.description4[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription description={data.description5[language]} />
+        </ChallengeCard>
+      );
+      break;
+
     default:
       content = <View />;
   }
