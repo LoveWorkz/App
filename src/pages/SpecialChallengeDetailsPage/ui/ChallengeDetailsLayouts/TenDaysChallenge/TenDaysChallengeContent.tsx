1382ac7b6bb448189016f5ba894d4ede80f092c0
Rustem Baltiyev <r.180200@gmail.com>
2023-11-21 11:20:56 +0300

    added special challenge "10 days"

--- src/pages/SpecialChallengeDetailsPage/ui/ChallengeDetailsLayouts/TenDaysChallenge/TenDaysChallengeContent.tsx
+++ src/pages/SpecialChallengeDetailsPage/ui/ChallengeDetailsLayouts/TenDaysChallenge/TenDaysChallengeContent.tsx
@@ -1,7 +1,13 @@
 import React, {memo} from 'react';
 import {View} from 'react-native';
 
-import {ChallengeIntroCard} from '@src/entities/Challenge';
+import {
+  ChallengeCard,
+  ChallengeDescription,
+  ChallengeIntroCard,
+} from '@src/entities/Challenge';
+import {LanguageValueType} from '@src/widgets/LanguageSwitcher';
+import {useTranslation} from 'react-i18next';
 
 interface TenDaysChallengeContentProps {
   data: any;
@@ -12,6 +18,9 @@ export const TenDaysChallengeContent = (
 ) => {
   const {data} = props;
 
+  const {i18n} = useTranslation();
+  const language = i18n.language as LanguageValueType;
+
   let content;
 
   switch (data.type) {
@@ -20,6 +29,35 @@ export const TenDaysChallengeContent = (
         <ChallengeIntroCard title={data.title} description={data.description} />
       );
       break;
+
+    case 'data_1':
+      content = (
+        <ChallengeCard title={data.title[language]}>
+          <ChallengeDescription
+            description={data.description1[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription description={data.description2[language]} />
+        </ChallengeCard>
+      );
+      break;
+
+    case 'data_2':
+      content = (
+        <ChallengeCard title={data.title[language]}>
+          <ChallengeDescription
+            description={data.description1[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription
+            description={data.description2[language]}
+            isMarginBottom
+          />
+          <ChallengeDescription description={data.description3[language]} />
+        </ChallengeCard>
+      );
+      break;
+
     default:
       content = <View />;
   }
