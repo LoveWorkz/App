12c6fc90af59e40b1547b73af71041ef12d0073b
Łukasz Kubok <lukaszkubok@ukaszs-MacBook-Air.local>
2024-08-08 15:31:37 +0200

    checking for color scheme on app start

--- src/app/providers/themeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/themeProvider/ui/ThemeProvider.tsx
@@ -3,15 +3,21 @@ import React, {ReactElement, useEffect, useMemo, useState} from 'react';
 import {themeStorage} from '@src/shared/lib/storage/adapters/themeAdapter';
 import {THEME_STORAGE_KEY} from '@src/shared/consts/storage';
 import {Theme, ThemeContext} from '../lib/ThemeContext';
+import {useColorScheme} from 'react-native';
 
 interface ThemeProviderProps {
   children: ReactElement;
 }
 
 export const ThemeProvider = (props: ThemeProviderProps) => {
+  const colorScheme = useColorScheme();
   const {children} = props;
 
-  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+  const defaultThemeOnFirstAppLaunch =
+    colorScheme === 'dark' ? Theme.Dark : Theme.LIGHT;
+
+  // const [theme, setTheme] = useState<Theme>(Theme.Dark);
+  const [theme, setTheme] = useState(defaultThemeOnFirstAppLaunch);
 
   useEffect(() => {
     const getDefaultTheme = async () => {
