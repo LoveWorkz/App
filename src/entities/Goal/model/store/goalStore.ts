3086357c1822a48e58eec0715df89db7b525837a
Łukasz Kubok <lukasz.kubok@techwings.com>
2024-08-30 14:36:34 +0200

    BLINE-312: user is able to chhoose max 5 goals

--- src/entities/Goal/model/store/goalStore.ts
+++ src/entities/Goal/model/store/goalStore.ts
@@ -111,6 +111,11 @@ class GoalStore {
       const selectedGoalsIds = this.selectedGoalsIds;
       let newSelectedGoalsIds = selectedGoalsIds;
 
+      // BLINE-312: User can select up to 5 goals
+      if (selectedGoalsIds.length >= 5) {
+        return;
+      }
+
       if (selectedGoalsIds.includes(id)) {
         newSelectedGoalsIds = selectedGoalsIds.filter(goal => goal !== id);
       } else {
